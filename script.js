52a1affb5abfa6f8108401d03ad30f76dd23b044
Code <52911973+coderrorOps@users.noreply.github.com>
2019-08-20 10:16:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-l
+document.onloadstart = test;
+document.onselectstart = test;
+document.oncontextmenu = test;
+function test() {
+    return false
+}
+
+function log_in() { 
+var correct_password = "Rubic123";
+  
+var password = document.getElementById("pass").value;
+  
+if (correct_password == password){
+document.location.href = "https://coderrorops.github.io/docs/index.html";
+}
