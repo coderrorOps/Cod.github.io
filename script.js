81cbf588f6b36823066616f28edc6a80404eb18d
Code <52911973+coderrorOps@users.noreply.github.com>
2019-08-20 10:21:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ var correct_password = "Rubic123";
 var password = document.getElementById("pass").value;
   
 if (correct_password == password){
-document.location.href = "docs/index.html";
+window.location.href = "docs/index.html";
 }
