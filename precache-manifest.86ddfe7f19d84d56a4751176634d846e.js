3ad5943ccc941aae4a682632074f0bd6fa9b2682
Coderror <52911973+coderrorOps@users.noreply.github.com>
2019-10-29 10:57:21 +0300

    TEST

---
+++ precache-manifest.86ddfe7f19d84d56a4751176634d846e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a33de6c56ed49e76bd5333a1d8e0c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bdcda24ca6eb779803b",
+    "url": "/static/css/main.6f19b52c.chunk.css"
+  },
+  {
+    "revision": "d638ed0f320c3105a758",
+    "url": "/static/js/2.fb1bec84.chunk.js"
+  },
+  {
+    "revision": "5bdcda24ca6eb779803b",
+    "url": "/static/js/main.1d58a68b.chunk.js"
+  },
+  {
+    "revision": "79068a36b4c3188650de",
+    "url": "/static/js/runtime-main.8c509df9.js"
+  }
+]);
